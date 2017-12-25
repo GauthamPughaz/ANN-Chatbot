495969efec285164ee4f8144b76f0db16e6a01aa
GauthamPughaz <gauthamleo55@gmail.com>
2017-12-25 12:43:58 +0530

    Updated chatbot.py .

--- chatbot.py
+++ chatbot.py
@@ -10,13 +10,22 @@ import json
 import datetime
 import numpy as np
 import time
+import random
 from nltk.stem.lancaster import LancasterStemmer
 
 
 # Creating a stemmer object.
 stemmer = LancasterStemmer()
+
 classes, words, documents = [], [], []
 ignore_words = ['?', ',', '.']
+training, output, training_data = [], [], []
+
+
+# Loading training data.
+with open('data.json') as data:
+    training_data = json.load(data)['data']
+
 
 # Normalizing using sigmoid function.
 def sigmoid(x):
@@ -28,9 +37,9 @@ def sigmoid_to_derivative(output):
     return output * (1 - output)
 
 
-# Train Artificial Neural Network
+# Train the Artificial Neural Network.
 def train(X, y, hidden_neurons=10, alpha=1, epochs=50000):
-    print ("Training with {0} neurons, alpha: {1}, dropout:{2} {3}".format(hidden_neurons, str(alpha), dropout, dropout_percent if dropout else '') )
+    print ("Training with {0} neurons, alpha: {1}".format(hidden_neurons, str(alpha)))
     print ("Input matrix: {}x{}    Output matrix: {}x{}".format(len(X),len(X[0]),1, len(classes)))
     np.random.seed(1)
 
@@ -44,7 +53,7 @@ def train(X, y, hidden_neurons=10, alpha=1, epochs=50000):
 
     synapse_0_direction_count = np.zeros_like(synapse_0)
     synapse_1_direction_count = np.zeros_like(synapse_1)
-        
+
     for j in iter(range(epochs+1)):
 
         # Feed forward through layers 0, 1, and 2
@@ -60,24 +69,24 @@ def train(X, y, hidden_neurons=10, alpha=1, epochs=50000):
                 last_mean_error = np.mean(np.abs(layer_2_error))
             else:
                 print ("break:", np.mean(np.abs(layer_2_error)), ">", last_mean_error )
-                break 
-        
-        layer_2_delta = layer_2_error * sigmoid_output_to_derivative(layer_2)
+                break
+
+        layer_2_delta = layer_2_error * sigmoid_to_derivative(layer_2)
 
         layer_1_error = layer_2_delta.dot(synapse_1.T)
-.
-        layer_1_delta = layer_1_error * sigmoid_output_to_derivative(layer_1)
-        
+
+        layer_1_delta = layer_1_error * sigmoid_to_derivative(layer_1)
+
         synapse_1_weight_update = (layer_1.T.dot(layer_2_delta))
         synapse_0_weight_update = (layer_0.T.dot(layer_1_delta))
-        
+
         if(j > 0):
             synapse_0_direction_count += np.abs(((synapse_0_weight_update > 0)+0) - ((prev_synapse_0_weight_update > 0) + 0))
-            synapse_1_direction_count += np.abs(((synapse_1_weight_update > 0)+0) - ((prev_synapse_1_weight_update > 0) + 0))        
-        
+            synapse_1_direction_count += np.abs(((synapse_1_weight_update > 0)+0) - ((prev_synapse_1_weight_update > 0) + 0))
+
         synapse_1 += alpha * synapse_1_weight_update
         synapse_0 += alpha * synapse_0_weight_update
-        
+
         prev_synapse_0_weight_update = synapse_0_weight_update
         prev_synapse_1_weight_update = synapse_1_weight_update
 
@@ -93,12 +102,11 @@ def train(X, y, hidden_neurons=10, alpha=1, epochs=50000):
 
     with open(synapse_file, 'w') as outfile:
         json.dump(synapse, outfile, indent=4, sort_keys=True)
-print ("saved synapses to:", synapse_file)
+    print ("Saved synapses to:", synapse_file)
 
 
 def prepare_data():
-    with open('data.json') as data:
-        training_data = json.load(data)['data']
+    global words, classes, documents, ignore_words, training_data
 
     # Tokenize the sentences in each class.
     for pattern in training_data:
@@ -116,8 +124,6 @@ def prepare_data():
     print(len(words), 'words')
 
     # Creating training data using one hot encoding method.
-    training, output = [], []
-
     # Creating bag of words for each sentence.
     for doc in documents:
         bag = []
@@ -132,6 +138,7 @@ def prepare_data():
 
     print(documents[0][0], ' --> ', training[0])
     print(documents[0][1], ' --> ', output[0])
+    return training, output
 
 
 def clean_sentence(sentence):
@@ -151,9 +158,47 @@ def bag_of_words(sentence, words):
 
 
 def predict(sentence):
+    # Loading the calculated synapse values.
+    synapse_file = 'synapses.json'
+    with open(synapse_file) as data_file:
+        synapse = json.load(data_file)
+        synapse_0 = np.asarray(synapse['synapse0'])
+        synapse_1 = np.asarray(synapse['synapse1'])
     x = bag_of_words(sentence, words)
+
     # Defining layers for prediction.
     l0 = x
     l1 = sigmoid(np.dot(l0, synapse_0))
     l2 = sigmoid(np.dot(l1, synapse_1))
     return l2
+
+
+def classify(sentence):
+    ERROR_THRESHOLD = 0.2
+    results = predict(sentence)
+
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD ]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_results =[[classes[r[0]],r[1]] for r in results]
+    print("Sentence: {}, Classification: {}".format(sentence, return_results))
+    return return_results
+
+
+if __name__ == '__main__':
+    # Preparing data.
+    X, y = prepare_data()
+    X = np.array(training)
+    y = np.array(output)
+
+    # Training the ANN.
+    start_time = time.time()
+    train(X, y, hidden_neurons=20, alpha=0.1, epochs=100000)
+    elapsed_time = time.time() - start_time
+    print ("Training time:", elapsed_time, "seconds")
+
+    # Classifying new sentence.
+    class_ = classify("Hi buddy...")[0][0]
+
+    for _class in training_data:
+        if _class["class"] == class_:
+            print(random.choice(_class["responses"]))
